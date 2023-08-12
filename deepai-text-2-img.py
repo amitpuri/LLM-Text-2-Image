7c4b7a220a153700478a4700e0743a03980839a8
Amit Puri <e.amitpuri@gmail.com>
2023-08-13 00:35:22 +0530

    update

--- deepai-text-2-img.py
+++ deepai-text-2-img.py
@@ -5,11 +5,21 @@ load_dotenv()
 
 deepai_api_key = os.getenv("DEEPAI_API_KEY")
 
-response = requests.post(
-    "https://api.deepai.org/api/text2img",
-    data={
-        'text': 'While traveling through a dense forest, you stumble upon an ancient, overgrown path veering off from the main trail. Do you dare to explore its mysteries?',
-    },
-    headers={'api-key': deepai_api_key}
+def text_to_image(prompt: str):
+    response = requests.post(
+        "https://api.deepai.org/api/text2img",
+        data={
+            'text': prompt,
+        },
+        headers={'api-key': deepai_api_key}
 )
 print(response.json())
+
+
+def main():
+    prompt = 'While traveling through a dense forest, you stumble upon an ancient, overgrown path veering off from the main trail. Do you dare to explore its mysteries?'
+    image_url = text_to_image(prompt)
+
+
+if __name__ == '__main__':
+    main()
