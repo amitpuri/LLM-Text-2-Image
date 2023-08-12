c0766a82552ff60925044b3c2b553ab9f46a06ba
Amit Puri <e.amitpuri@gmail.com>
2023-08-13 01:39:54 +0530

    Update

--- openai-text-2-img.py
+++ openai-text-2-img.py
@@ -8,10 +8,10 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_org_id = os.getenv("OPENAI_ORG_ID")
 openai_api_base = "https://api.openai.com/v1"
-azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+
 azure_openai_api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
-deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
-use_azure = False
+azure_openai_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+use_azure = True
 
 def text_to_image(prompt: str):
    authorization = f"Bearer {openai_api_key}"
@@ -25,26 +25,28 @@ def create_image_from_prompt(prompt: str, imagesize: str ="256x256", num_images:
    if use_azure:            
       openai.api_type = "azure"        
       openai.api_version = "2023-05-15" 
-      openai.api_base = azure_openai_api_base 
-      openai.deployment_name = deployment_name  
       openai.api_key = azure_openai_api_key
+      openai.api_base = azure_openai_api_base
    else:
       openai.api_type = "openai"        
       openai.api_version = '2020-11-07'
       openai.api_key = openai_api_key
+      openai.api_base = openai_api_base
       if openai_org_id: 
          openai.organization = openai_org_id
-      response = openai.Image.create(
+   
+   response = openai.Image.create(
                     prompt=prompt,
                     n=num_images,
                     size=imagesize)
-      image_urls = [data['url'] for data in response['data']]
-      return image_urls
+   image_urls = [data['url'] for data in response['data']]
+   return image_urls
 
 def main():
    prompt = 'While traveling through a dense forest, you stumble upon an ancient, overgrown path veering off from the main trail. Do you dare to explore its mysteries?'
-   image_url = create_image_from_prompt(prompt)
-   print(image_url)
+   image_urls = create_image_from_prompt(prompt)
+   for image_url in image_urls:
+      print(image_url)
 
 if __name__ == '__main__':
     main()
